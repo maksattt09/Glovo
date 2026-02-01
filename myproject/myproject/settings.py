4937407189f5793898978d6b86a32411264fdeff
Maksattt00 <joldoshevmaksat@icloud.com>
2026-02-01 10:51:46 +0600

    ff

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -128,7 +128,8 @@ MODELTRANSLATION_LANGUAGES = ('en', 'ru')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_PATH = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 MEDIA_PATH = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
